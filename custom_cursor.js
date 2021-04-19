0bdf4ee482f45555f8663cf449dffe1d92003c78
tomaszew95 <50740828+tomaszew95@users.noreply.github.com>
2021-04-19 16:00:08 +0200

    correction - adding function()

--- custom_cursor.js
+++ custom_cursor.js
@@ -1,10 +1,13 @@
 var customCursor = document.getElementById("ceros-custom-cursor-plugin");
 var hoverObject = customCursor.getAttribute("hover-object").split(" ");
 var cursorType = customCursor.getAttribute("cursor-type").split(" ");
-if(hoverObject.length != cursorType.length){
-    console.warn("not equal number of Configurable Parameters");
-    return;
+var customCur = () => {
+    if(hoverObject.length != cursorType.length){
+        console.warn("not equal number of Configurable Parameters");
+        return;
+    }
+    for(let i=0;i<hoverObject.length;i++){
+        $(`hoverObject[i]`).attr("cursor", cursorType[i].toString());
+    }
 }
-for(let i=0;i<hoverObject.length;i++){
-    $(`hoverObject[i]`).attr("cursor", cursorType[i].toString());
-}
+customCur;
