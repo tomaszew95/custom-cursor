9aee0644817dee4cbd70542fae5b484bf3d560e7
tomaszew95 <50740828+tomaszew95@users.noreply.github.com>
2021-04-19 17:20:29 +0200

    $()

--- custom_cursor.js
+++ custom_cursor.js
@@ -1,7 +1,7 @@
 var customCursor = document.getElementById("ceros-custom-cursor-plugin");
 var hoverObject = customCursor.getAttribute("hover-object").split(" ");
 var cursorType = customCursor.getAttribute("cursor-type").split(" ");
-let hoverObj;
+var hoverObj;
 var customCur = () => {
     if(hoverObject.length != cursorType.length){
         console.warn("not equal number of Configurable Parameters");
@@ -10,7 +10,7 @@ var customCur = () => {
     for(let i=0;i<hoverObject.length;i++){
         hoverObj = document.querySelectorAll(hoverObject[i]);
         // hoverObj[i] = document.querySelectorAll(hoverObject[i].toString());
-        hoverObj.attr("style", 'cursor:' + cursorType[i]);
+        $(hoverObj).attr("style", 'cursor:' + cursorType[i]);
         console.log($(hoverObj));
     }
 }
