06f7ed52c83049c74f91a2271007d54e7d30f0e5
tomaszew95 <50740828+tomaszew95@users.noreply.github.com>
2021-04-19 20:17:46 +0200

    adjustment

--- custom_cursor.js
+++ custom_cursor.js
@@ -1,17 +1,39 @@
 var customCursor = document.getElementById("ceros-custom-cursor-plugin");
 var hoverObject = customCursor.getAttribute("hover-object").split(" ");
 var cursorType = customCursor.getAttribute("cursor-type").split(" ");
-var hoverObj;
-var customCur = () => {
-    if(hoverObject.length != cursorType.length){
-        console.warn("not equal number of Configurable Parameters");
-        return;
-    }
-    for(let i=0;i<hoverObject.length;i++){
-        hoverObj = document.querySelectorAll(hoverObject[i]);
-        // hoverObj[i] = document.querySelectorAll(hoverObject[i].toString());
-        $(hoverObj).attr("style", 'cursor:' + cursorType[i]);
-        console.log($(hoverObj));
-    }
-}
-customCur();
+(function(){
+    'use strict'; 
+    require.config({ 
+        paths: { 
+            CerosSDK: '//sdk.ceros.com/standalone-player-sdk-v5.min' 
+        } 
+    }); 
+    require(['CerosSDK'], function (CerosSDK) { 
+        CerosSDK.findExperience() 
+            .fail(function (error) { 
+                console.error(error); 
+            }) 
+            .done(function (experience) { 
+                window.myExperience = experience;
+
+                experience.on(CerosSDK.EVENTS.PAGE_CHANGED, pageChangedCallback);
+                function pageChangedCallback(){
+                    let hoverObj;
+                    if(hoverObject.length != cursorType.length){
+                        console.warn("not equal number of Configurable Parameters");
+                        return;
+                    }
+                    for(let i=0;i<hoverObject.length;i++){
+                        if(hoverObject[i].charAt(0)=="#"){
+                            let idd = hoverObject[i].slice(1);
+                            hoverObj = document.querySelectorAll("[id='" + idd + "']");
+                        }
+                        else{
+                            hoverObj = document.querySelectorAll(hoverObject[i]);
+                        }
+                        $(hoverObj).css({cursor: cursorType[i]});
+                    }
+                }
+            }) 
+    }); 
+})();
